555b2753b301a03e74a1c4bb95cfbae405a101a6
unknown <basak.amasya@aalto.fi>
2022-12-06 14:20:38 +0200

    1.13 implemented

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -20,14 +20,23 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState([0,0,0,0,0,0,0])
 
   const randomAnecdote = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length)) //anecdotes.length is the max
   }
 
+  const vote = () => {
+    const copy = { ...points }
+    copy[selected] += 1
+    setPoints(copy)
+  }
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <Button onClick={vote} text="vote" />
       <Button onClick={randomAnecdote} text="next anecdote" />
     </div>
   )
