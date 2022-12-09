ee96e56521bc9efbc0e6de919d403f22112332ac
unknown <basak.amasya@aalto.fi>
2022-12-09 15:08:44 +0200

    2.14 implemented

--- part2/countries/src/components/SingleCountry.js
+++ part2/countries/src/components/SingleCountry.js
@@ -1,5 +1,25 @@
-const SingleCountry = (props) =>   
-<>
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const SingleCountry = (props) =>  {
+  const [weather, setWeather] = useState("")
+
+  const capital = props.countriesToShow[0].capital[0]
+  console.log(capital)
+
+  const hook = () => {
+    axios
+      .get('https://wttr.in/'+capital+'.png')
+      .then(response => {
+        console.log('promise fulfilled')
+        setWeather(response.data)
+        console.log(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
+  return (
+    <>
   {props.countriesToShow.map(country => 
     <div key={country.name.common}>
     <h1>{country.name.common}</h1>
@@ -13,9 +33,13 @@ const SingleCountry = (props) =>
       </ul>
 
     <p>{country.flag}</p>
+    <h1>Weather in {country.capital}</h1>
+    <img src={"https://wttr.in/"+ country.capital +'.png'} ></img>
     </div>  
   )}
+  </>
+  )
+} 
 
-</>
 
 export default SingleCountry
