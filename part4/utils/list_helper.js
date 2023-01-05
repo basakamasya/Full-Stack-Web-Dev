9e5596a52724482d4301c9f318ebeb78c32c561a
unknown <basak.amasya@aalto.fi>
2023-01-05 15:53:36 +0300

    4.4 implemented

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (array) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+
+  return array.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
