361d26ad9003459343cef5607e25b41edf44dc9a
unknown <basak.amasya@aalto.fi>
2023-01-06 15:29:09 +0300

    4.15 implemented

--- part4/app.js
+++ part4/app.js
@@ -4,6 +4,7 @@ const app = express()
 require('express-async-errors')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -22,5 +23,6 @@ app.use(express.static('build'))
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
