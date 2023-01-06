a4a7d09583ac1bb919b8a04bd9f7c3ed11fad499
unknown <basak.amasya@aalto.fi>
2023-01-06 11:09:34 +0300

    4.10 implemented

--- part4/tests/api.test.js
+++ part4/tests/api.test.js
@@ -33,6 +33,33 @@ test('the unique identifier property of the blog posts is named id', async () =>
   })
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'New blog post coming your way',
+    author: 'BA',
+    url: '/new',
+    likes: 0,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(r => r.title)
+  const authors = response.body.map(r => r.author)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1) //checking the length is increased by one
+
+  expect(titles).toContain( //checking the title of the post
+    'New blog post coming your way'
+  )
+  expect(authors).toContain( //checking the author of the post
+    'BA'
+  )
+})
 
 afterAll(() => {
   mongoose.connection.close()
