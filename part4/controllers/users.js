f55bafe575e754b0e1e49503aa60149332da3a8e
unknown <basak.amasya@aalto.fi>
2023-01-12 20:49:01 +0200

    4.16 implemented

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -1,9 +1,38 @@
+//taken from the given example
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
+
+  if (!username) {
+    return response.status(400).json({
+      error: 'username must be given'
+    })
+  }
+  if (!password) {
+    return response.status(400).json({
+      error: 'password must be given'
+    })
+  }
+  else if (password.length < 3) {
+    return response.status(400).json({
+      error: 'password must be at least 3 characters long'
+    })
+  }
+  else if (username.length < 3) {
+    return response.status(400).json({
+      error: 'username must be at least 3 characters long'
+    })
+  }
+
+  const existingUser = await User.findOne({ username }) //the username must be unique
+  if (existingUser) {
+    return response.status(400).json({
+      error: 'username must be unique'
+    })
+  }
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
