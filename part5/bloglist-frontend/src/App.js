d71f88c014b0f4280c005c99d0bc8d2df19778f8
unknown <basak.amasya@aalto.fi>
2023-01-19 19:09:03 +0200

    5.9 implemented

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -127,6 +127,8 @@ const App = () => {
       })
   }
 
+  const compareLikes = (blog1, blog2) => {return blog2.likes - blog1.likes}
+
   if (user === null) {
     return (
       <div>
@@ -153,7 +155,7 @@ const App = () => {
       <Togglable buttonLabel='create new blog' ref={blogFormRef}>
         <BlogForm createBlog={addBlog}/>
       </Togglable>
-      {blogs.map(blog =>
+      {blogs.sort(compareLikes) && blogs.map(blog =>
         <Blog key={blog.id} blog={blog} updateBlog={() => updateBlog(blog.id)} />
       )}
     </div>
