c9567a95ffb2a9b6699cd23fec7a0711c942c7b8
unknown <basak.amasya@aalto.fi>
2023-01-22 13:28:12 +0200

    5.14 implemented

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders blog', () => {
+const blog = {
+  title: 'Test',
+  author: 'BA',
+  likes: 0,
+  url: 'test',
+  user: {
+    name: 'BA',
+    username: 'bamasya',
+  },
+}
 
-  const blog = {
-    title: 'Test',
-    author: 'BA',
-    likes: 0,
-    url: 'test',
-    user: {
-      name: 'BA',
-      username: 'bamasya',
-    },
-  }
+
+test('renders blog', () => {
 
   const container = render(<Blog blog={blog} />)
 
@@ -32,6 +34,31 @@ test('renders blog', () => {
 
   const urlAndLikes = container.getByTestId('extra-info')
   expect(urlAndLikes).toHaveStyle(
+    'display: none;'
+  )
+
+})
+
+test('clicking the view button displays url and likes', async () => {
+  const container = render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = container.getByTestId('view-button')
+  await user.click(button)
+
+  const url = container.getByTestId('url')
+  expect(url).toHaveTextContent(
+    'test'
+  )
+  expect(url).not.toHaveStyle(
+    'display: none;'
+  )
+
+  const likes = container.getByTestId('likes')
+  expect(likes).toHaveTextContent(
+    '0'
+  )
+  expect(likes).not.toHaveStyle(
     'display: none;'
   )
 
