52e4c4f4360144b0d41355cbad652b42ff2f1702
unknown <basak.amasya@aalto.fi>
2023-01-26 16:30:07 +0200

    5.17 implemented

--- part4/index.js
+++ part4/index.js
@@ -1,13 +1,13 @@
 const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const cors = require('cors')
-const mongoose = require('mongoose')
+// const cors = require('cors')
+// const mongoose = require('mongoose')
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+// const mongoUrl = process.env.MONGODB_URI
+// mongoose.connect(mongoUrl)
 
-app.use(cors())
+// app.use(cors())
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
