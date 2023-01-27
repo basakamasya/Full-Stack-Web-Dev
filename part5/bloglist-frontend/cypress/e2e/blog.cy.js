d10a9cb51263521a7cbbca90e2251120c6847cc6
unknown <basak.amasya@aalto.fi>
2023-01-27 16:19:32 +0200

    5.22 implemented

--- part5/bloglist-frontend/cypress/e2e/blog.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog.cy.js
@@ -1,7 +1,7 @@
-describe('Blog app', function() {
-  beforeEach(function() {
+describe('Blog app', function () {
+  beforeEach(function () {
     cy.request('POST', 'http://localhost:3000/api/testing/reset')
-    //create a user
+
     const user = {
       name: 'Burak',
       username: 'burak',
@@ -11,14 +11,14 @@ describe('Blog app', function() {
     cy.visit('http://localhost:3000/')
   })
 
-  it('Login form is shown', function() {
+  it('Login form is shown', function () {
     cy.contains('Log in to application')
     cy.contains('username')
     cy.contains('password')
   })
 
-  describe('Login',function() {
-    it('succeeds with correct credentials', function() {
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
       cy.get('#username').type('burak')
       cy.get('#password').type('sifre')
       cy.get('#login-button').click()
@@ -26,7 +26,7 @@ describe('Blog app', function() {
       cy.contains('Burak logged in')
     })
 
-    it('fails with wrong credentials', function() {
+    it('fails with wrong credentials', function () {
       cy.get('#username').type('burak')
       cy.get('#password').type('wrong')
       cy.get('#login-button').click()
@@ -40,15 +40,15 @@ describe('Blog app', function() {
     })
   })
 
-  describe('When logged in', function() {
-    beforeEach(function() {
+  describe('When logged in', function () {
+    beforeEach(function () {
       cy.get('#username').type('burak')
       cy.get('#password').type('sifre')
       cy.get('#login-button').click()
 
     })
 
-    it('A blog can be created', function() {
+    it('A blog can be created', function () {
       cy.contains('create new blog').click()
       cy.get('#title').type('a blog created by cypress')
       cy.get('#author').type('cypress')
@@ -57,7 +57,7 @@ describe('Blog app', function() {
       cy.contains('a blog created by cypress')
     })
 
-    it('A blog can be created and liked', function() {
+    it('A blog can be created and liked', function () {
       cy.contains('create new blog').click()
       cy.get('#title').type('a brand new blog created by cypress')
       cy.get('#author').type('cypress')
@@ -71,7 +71,7 @@ describe('Blog app', function() {
       cy.contains('likes 1')
     })
 
-    it('A blog can be created and deleted by the user who created', function() {
+    it('A blog can be created and deleted by the user who created', function () {
       cy.contains('create new blog').click()
       cy.get('#title').type('very special')
       cy.get('#author').type('cypress')
@@ -85,6 +85,34 @@ describe('Blog app', function() {
       cy.on('windows:confirm', () => true)
       cy.get('html').should('not.contain', 'very special')
     })
+
+    it('Blogs are ordered correctly according to likes', function () {
+      cy.contains('create new blog').click()
+      cy.get('#title').type('incredible blog')
+      cy.get('#author').type('cypress')
+      cy.get('#url').type('/cypress5')
+      cy.get('#create').click()
+      cy.contains('incredible blog')
+      cy.wait(1000)
+
+      cy.get('#view').click()
+      cy.contains('likes 0')
+      cy.get('#like').click()
+      cy.contains('likes 1')
+
+      cy.contains('create new blog').click()
+      cy.get('#title').type('very amazing blog')
+      cy.get('#author').type('cypress')
+      cy.get('#url').type('/cypress6')
+      cy.get('#create').click()
+      cy.contains('very amazing blog')
+      cy.wait(1000)
+
+      cy.get('.blog').eq(0).should('contain', 'incredible blog')
+      cy.get('.blog').eq(1).should('contain', 'very amazing blog cypress')
+
+    })
+
 
   })
 
