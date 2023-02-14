de88786ef3ae65c948c6f520e8f78d79e301a959
unknown <basak.amasya@aalto.fi>
2023-02-14 10:18:37 +0300

    6.20 implemented (wrongly numbered as 6.19 in e part)

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,18 +1,30 @@
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = (props) => {
+  //const notification = useSelector(state => state.notification)
+  const notification = props.notification
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
   if (notification)
-  return (
-    <div style={style}>
-      {notification}
-    </div>
-  )
+    return (
+      <div style={style}>
+        {notification}
+      </div>
+    )
 }
 
-export default Notification
+//export default Notification
+
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification,
+    anecdotes: state.anecdotes,
+    filter: state.filter,
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
